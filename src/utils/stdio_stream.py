9109e736fe111a16478c08035792018afd7b87c5
NuclearManD <dylanbrophy@gmail.com>
2021-08-04 16:39:03 -0700

    stdio bugfixes

--- src/utils/stdio_stream.py
+++ src/utils/stdio_stream.py
@@ -1,14 +1,15 @@
 
-import sys
+import sys, select
 
 
 class StdIOStream:
 
     def write(self, data):
-        sys.stdout.write(data)
+        sys.stdout.buffer.write(data)
+        sys.stdout.buffer.flush()
 
     def available(self):
         return select.select([sys.stdin,],[],[],0.0)[0]
 
     def read(self):
-        return sys.stdin.read()
+        return sys.stdin.buffer.read(1)
