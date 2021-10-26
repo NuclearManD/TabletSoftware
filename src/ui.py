e6245d20f844be7275015b39cfc4292ce2836bdb
NuclearManD <dylanbrophy@gmail.com>
2021-10-26 07:47:31 -0400

    Start of a UI framework

--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,10 @@
 
-import time, math, traceback
+import time
+import traceback
+from typing import List
+
+from utils.protocol import TabletInterface, TabletDisplay
+
 
 class PopupBox:
 
@@ -68,7 +73,7 @@ class PopupBox:
                 x = point[0]
                 y = point[1]
 
-                if x >= left and x <= right and y >= button_top and y <= button_bottom:
+                if left <= x <= right and y >= button_top and y <= button_bottom:
                     # press
                     button_id = ((x - left) * len(self.buttons)) // self.width
 
@@ -89,3 +94,157 @@ class ErrorPopupBox(PopupBox):
 
         super().__init__(width, height, ["Ok"], content, bgcolor=0x3030F0)
 
+
+class UIElement:
+    def __init__(self, x, y):
+        """x and y arguments are the location of the element (center of element is subclass dependant)"""
+        self.x = x
+        self.y = y
+
+    def update(self):
+        pass
+
+    def onPress(self, x, y):
+        pass
+
+    def onRelease(self, x, y):
+        pass
+
+    def onClick(self, x, y):
+        pass
+
+    def render(self, display: TabletDisplay, x, y):
+        """x and y arguments are the location of the upper-left corner of the window"""
+        raise NotImplementedError("Must be implemented in subclass")
+
+    def onDrag(self, x1, y1, x2, y2):
+        pass
+
+
+class ApplicationWindow:
+    # Maximum amount of time a click can last - the user must tap and untap within this time
+    # for it to count as a click.  Otherwise it becomes a drag.  Measured in seconds.
+    PRESS_CLICK_TIME_CUTOFF = 0.5
+
+    def __init__(self, elements: List[UIElement]):
+        self.elements = elements
+        self.keepRunningMainLoop = False
+        self.pressTimer = 0
+        self.wasPressed = False
+        self.lastPress = None
+
+    def update(self):
+        for i in self.elements:
+            i.update()
+
+    def render(self, display: TabletDisplay):
+        for i in self.elements:
+            i.render(display, 0, 0)
+
+    def mainloop(self, tablet: TabletInterface):
+        display = tablet.getDisplay()
+        self.render(display)
+
+        self.keepRunningMainLoop = True
+        while self.keepRunningMainLoop:
+            time.sleep(0.010)
+            presses = tablet.getPresses()
+            if len(presses) == 1:
+                press = presses[0]
+                if not self.wasPressed:
+                    self.wasPressed = True
+                    self.lastPress = press
+                    self.pressTimer = time.time()
+                    for i in self.elements:
+                        i.onPress(self.lastPress[0], self.lastPress[1])
+
+                elif self.pressTimer + self.PRESS_CLICK_TIME_CUTOFF < time.time():
+                    # Dragging the cursor
+                    for i in self.elements:
+                        i.onDrag(self.lastPress[0], self.lastPress[1], press[0], press[1])
+                    self.lastPress = press
+
+            elif len(presses) == 0:
+                # Unpress
+                if self.lastPress is not None:
+                    for i in self.elements:
+                        i.onRelease(self.lastPress[0], self.lastPress[1])
+                    if self.wasPressed and self.pressTimer + self.PRESS_CLICK_TIME_CUTOFF > time.time():
+                        # Click
+                        for i in self.elements:
+                            i.onClick(self.lastPress[0], self.lastPress[1])
+
+                self.wasPressed = False
+
+            else:
+                # Multitouch
+                self.wasPressed = False
+                for point in presses:
+                    x = point[0]
+                    y = point[1]
+
+                    # What do we do with multitouch?
+
+        print('Goodbye!')
+
+    def stopMainLoop(self):
+        """
+        You can easily use this as the callback for a button, as long as you don't have cleanup to do before
+        your application exits.
+        """
+        print('Stopping app.')
+        self.keepRunningMainLoop = False
+
+
+class TextButtonElement(UIElement):
+    def __init__(self, x, y, width, height,
+                 text: str, color, callback = None, xPad = 3, yPad = 2,
+                 pressColor=None, display: TabletDisplay = None,
+                 textColor=0xFFFFFF):
+        """x and y arguments are the location of the element"""
+        super().__init__(x, y)
+        self.width = width
+        self.height = height
+        self.xPad = xPad
+        self.yPad = yPad
+        self.color = color
+        self.pressColor = pressColor
+        self.display = display
+        self.text = text
+        self.textColor = textColor
+        self.cb = callback
+
+    def onPress(self, x, y):
+        # Don't process the event if it's not in this button
+        if x < self.x or y < self.y or x > self.x + self.width or y > self.y + self.width:
+            return
+
+        if self.pressColor is not None and self.display is not None:
+            self.display.fillRect(self.x, self.y, self.width, self.height, self.pressColor)
+            self.display.setCursor(self.x + self.xPad, self.y + self.yPad)
+            self.display.setTextColor(self.textColor)
+            self.display.writeText(self.text)
+
+    def onRelease(self, x, y):
+        # Don't process the event if it's not in this button
+        if x < self.x or y < self.y or x > self.x + self.width or y > self.y + self.width:
+            return
+
+        if self.pressColor is not None and self.display is not None:
+            self.render(self.display, 0, 0)
+
+    def onClick(self, x, y):
+        # Don't process the event if it's not in this button
+        if x < self.x or y < self.y or x > self.x + self.width or y > self.y + self.width:
+            return
+
+        self.cb(self)
+
+    def render(self, display: TabletDisplay, x, y):
+        """x and y arguments are the location of the upper-left corner of the window"""
+        self.display = display
+
+        display.fillRect(self.x, self.y, self.width, self.height, self.color)
+        display.setCursor(self.x + self.xPad, self.y + self.yPad)
+        display.setTextColor(self.textColor)
+        display.writeText(self.text)
